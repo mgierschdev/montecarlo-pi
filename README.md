5b204c901986c5ed290d8dc3bd5986cfb5028057
Maximiliam <mgierschmann@protonmail.com>
2020-12-07 13:41:07 -0400

    README update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Montercarlo-Pi
-Just for curiosity, generated the PI number by doing a Montecarlo simulation.
+Generated the PI number by doing a Montecarlo simulation.
 
-By generating 1000000000 random points in a plane we get the first 3 decimals of PI in around 1 min with my current PC, this could be optimized. We count the number of points that are within a square and circle in the plane, both the circle and the square in the plane have the property circle radio = square side length, by doing this and dividing both number circleStack / SquareStack, we get an approximation of PI.
+By generating 1000000000 random points in a plane we get the first 3 decimals of PI in around 1 min, this could be optimized. We count the number of points that are within a square and circle in the plane, both the circle and the square in the plane have the property circle radio = square side length, by doing this and dividing both number circleStack / SquareStack, we get an approximation of PI.
 
 
 Approximation plotted [Notebook script](./notebook-jupiter/montecarlopi.ipynb)
--- src/main/java/MonteCarlo.java
+++ src/main/java/MonteCarlo.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MonteCarlo {
     private double pi = 0;
     private double squareBucket = 1;
@@ -6,37 +10,48 @@ public class MonteCarlo {
     private final int CIRCLE_RADIO = 200;
     private final int[] SQUARE_CORNER = new int[]{200, 200};
 
-    public void simulatePi(long iterations){
+    public void simulatePi(long iterations) {
         pi = 0;
-        System.out.println("Iterations "+iterations);
-        for(long i = 0; i < iterations; i++){
-            double[] coordinate = new double[]{(Math.random() * 800), (Math.random() * 500)};
 
-            if(insideSquare(coordinate)){
-                squareBucket++;
+        try {
+            File file = new File("pi.cvs");
+            FileWriter fr = new FileWriter(file, true);
+            fr.write("iteration,pi\n");
+
+            for (long i = 0; i < iterations; i++) {
+                double[] coordinate = new double[]{(Math.random() * 800), (Math.random() * 500)};
+
+                if (insideSquare(coordinate)) {
+                    squareBucket++;
+                }
+
+                if (insideCircle(coordinate)) {
+                    circleBucket++;
+                }
+
+                pi = circleBucket / squareBucket;
+                fr.write(i + "," + pi + "\n");
             }
 
-            if(insideCircle(coordinate)){
-                circleBucket++;
-            }
-
-            pi = circleBucket / squareBucket;
+            fr.close();
+        } catch (IOException e) {
+            System.out.println(e);
         }
 
-        System.out.println("Generated after MAX_INTEGER "+iterations+" iterations "+ pi);
+        System.out.println("Generated after MAX_INTEGER " + iterations + " iterations " + pi);
     }
 
-    public boolean insideSquare(double[] coordinate){
-        if(coordinate[0] <= SQUARE_CORNER[0] && coordinate[1] <= SQUARE_CORNER[1]){
+    public boolean insideSquare(double[] coordinate) {
+        if (coordinate[0] <= SQUARE_CORNER[0] && coordinate[1] <= SQUARE_CORNER[1]) {
             return true;
         }
         return false;
     }
 
-    public boolean insideCircle(double[] coordinate){
-       double radio = Math.pow(CIRCLE_RADIO, 2);
-       double val = Math.pow(coordinate[0] - CIRCLE_CENTER[0], 2)  + Math.pow(coordinate[1] - CIRCLE_CENTER[1], 2);
-       return radio >= val;
+    public boolean insideCircle(double[] coordinate) {
+        double radio = Math.pow(CIRCLE_RADIO, 2);
+        double val = Math.pow(coordinate[0] - CIRCLE_CENTER[0], 2) + Math.pow(coordinate[1] - CIRCLE_CENTER[1], 2);
+        return radio >= val;
     }
 
     public String getPi() {
